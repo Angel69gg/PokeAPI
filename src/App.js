c6b50e17e9247c75690812ab32c9dfb43c0b424e
Angel <angelgamboarodgz@gmail.com>
2022-06-27 14:14:14 -0500

    feature: Added Pokemon names

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [id, setId] = useState(1)
+  const [lista, setLista] = useState([{}]);
+  const [pokemon, setPokemon] = useState({});
+  useEffect(() => {
+  const get = async () => {
+  const data = await axios.get("https://pokeapi.co/api/v2/pokemon/1")
+  setPokemon(data.data)
+  return data.data    
+  }
+  get()
+  },[]);
+  const obtenerInformacion =async (id) =>{
+    console.log(id)
+    const data= await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    setPokemon(data.data)
+    console.log(data.data)
+    let a= id + 1;
+    setId(a)
+  }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+    <div className="imagen" >
+        <h1 className="">{pokemon.name}</h1>  
+       {/*  <img src={pokemon.sprites.front_default} alt="pokemon" />   */}
+        </div>
+        <button onClick={() => obtenerInformacion(id)} className="boton">Siguiente</button>
     </div>
   );
 }
