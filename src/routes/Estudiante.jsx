01fbaeda40d02ef86709462649b1f427e99a5733
Angel <angelgamboarodgz@gmail.com>
2022-07-04 13:26:41 -0500

    feat: Added student about

--- src/routes/Estudiante.jsx
+++ src/routes/Estudiante.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const Estudiante=()=> {
   return (
-    <div>Estudiante</div>
+    <a href="#" class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">190547</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">Angel Manuel Gamboa Rodriguez </p>
+    <p class="font-normal text-gray-700 dark:text-gray-400">Universidad Tecnologica de Aguascalientes </p>
+    <p class="font-normal text-gray-700 dark:text-gray-400">Ingenieria en Desarrollo y Gestion de Software </p>
+    <p class="font-normal text-gray-700 dark:text-gray-400">Grupo 8A </p>
+</a>
   )
 }
